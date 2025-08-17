16bd43502431bdf13afc61a138b3b111df733c8c
AndyC00 <chenjinzhi96@gmail.com>
2025-08-18 11:56:54 +1200

    modified to better structure

---
+++ init.js
@@ -0,0 +1,8 @@
+// interactable planet init:
+window.addEventListener('DOMContentLoaded', () => {
+  initDrifter({
+    imgSrc: "_assets/Others/artificial satellite.png",
+    restitution: 0.98,
+    jitter: 8
+  });
+});
